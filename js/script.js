925d450ca6b872804d1782a323348174f6ebb3b7
Evelyn Spr <82214759+evelynspr@users.noreply.github.com>
2021-04-13 01:00:04 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const text = document.querySelector('.text');
-const text2 = document.querySelector('.text-2');
-
-window.addEventListener('scroll', function() {
-	var value = window.scrollY;
-
-	text.style.left = value * 0.5 + 'px';
-	text2.style.left = -value * 0.5 + 'px';
-});
